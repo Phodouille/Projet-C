25d3ab45c8cb35cb6693a305b607285240a3e7f9
Lucas NGUYEN <lucas.nguyen@efrei.net>
2023-04-18 18:28:43 +0200

    Crétatin des premier struct

--- main.c
+++ main.c
@@ -1,2 +1,45 @@
 #include <stdio.h>
 
+typedef struct 
+{
+    int pos_x;
+    int pos_y;
+}Point;
+
+
+Point *create_point(int px, int py)
+{
+    Point *p = (Point *)malloc(sizeof(Point));
+    p->pos_x = px;
+    p->pos_y = py;
+    return p;
+}
+
+typedef struct line 
+{
+    Point *p1;
+    Point *p2;
+}Line;
+
+
+
+void delete_point(Point * point)
+{
+    free(point);
+}
+
+void print_point(Point * p)
+{
+    printf("x = %d, y = %d \n", p->pos_x, p->pos_y);
+}
+
+int main()
+{
+    Point *p = create_point(10, 15  );
+    print_point(p);
+    delete_point(p);    
+    return 0;
+}
+
+
+
